6ac0bc8d0e4612ffc973bb569ccfd39316afd245
Adriel Renato <adriel.renato26@gmail.com>
2025-03-30 15:17:24 -0300

    Criando uma factory para criação dos objetos das entidades

---
+++ src/main/java/br/com/alura/codechella/domain/entities/usuario/Usuario.java
@@ -0,0 +1,48 @@
+package br.com.alura.codechella.domain.entities.usuario;
+
+import br.com.alura.codechella.domain.Endereco;
+
+import java.time.LocalDate;
+
+public class Usuario {
+    private String cpf;
+    private String nome;
+    private LocalDate nascimento;
+    private String email;
+    private Endereco endereco;
+
+    public Usuario(String cpf, String nome, LocalDate nascimento, String email) {
+        if (cpf == null || !cpf.matches("\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}")) {
+            throw new IllegalArgumentException("Cpf no padrão errado");
+        }
+
+        this.cpf = cpf;
+        this.nome = nome;
+        this.nascimento = nascimento;
+        this.email = email;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public LocalDate getNascimento() {
+        return nascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
